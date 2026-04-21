a5df808a3b8fe8468d253e1d6332ec3c78fc2676
variannn340 <varianvarian0301@gmail.com>
2026-04-21 14:13:31 +0700

    Full void input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -7,4 +7,27 @@ int x;
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen array (maksimal 10) : ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";            
+        }
+    }
+
+    cout << "\n============================\n";
+    cout << "        Masukkan Elemen Array   ";
+    cout << "==============================\n";
+    for (int i = 0; 1 < nPanjang; i++)
+    {
+            cout << "Data ke-" << (i + 1) << " = ";
+            cin >> element[i];
+    }
+}
+
