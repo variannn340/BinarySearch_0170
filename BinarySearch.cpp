d4de8f03ae20a87ee398a2e5c74ad8115b10b4e3
variannn340 <varianvarian0301@gmail.com>
2026-04-21 14:51:06 +0700

    Full void binarySearch

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -71,5 +71,41 @@ void display()
 
 void binarySearch()
 {
-    
+    char ulang;
+    do
+    {
+
+        cout << "\n=====================================\n";
+        cout << "      Pencarian Binary Search       \n";
+        cout << "====================================\n";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[v] Elemen " << x << " ditemukan pada indeks" << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid + 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+
+        } while (low <= high);
+
+        if (low <= high);
+        {
+            cout << "\nIngin mencari lagi? (y/n): ";
+            cin >> ulang;
+        }
+    } while (ulang == 'y' || ulang == 'Y');
 }
